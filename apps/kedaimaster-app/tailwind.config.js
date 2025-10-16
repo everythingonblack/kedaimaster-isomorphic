7e4f2e7600eafe5e260d264aa135766fce3896af
everythingonblack <pasaardianputra@gmail.com>
2025-10-17 00:56:28 +0700

    ok

--- apps/kedaimaster-app/tailwind.config.js
+++ apps/kedaimaster-app/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+const sharedConfig = require('tailwind-config');
+
 module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
     '../../packages/isomorphic-core/src/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {},
-  },
+  presets: [sharedConfig],
+  theme: {},
   plugins: [],
-}
+};
